27cb8eb8f777e63e32221b8627e1bcb6d6c4f70f
JulioCesar-A <145122101+JulioCesar-A@users.noreply.github.com>
2025-06-07 20:06:36 -0300

    Update tab2.page.ts
    Correção na interface ConsultaResponse

--- ioniccurrencyconverterapp/src/app/tab2/tab2.page.ts
+++ ioniccurrencyconverterapp/src/app/tab2/tab2.page.ts
@@ -2,16 +2,8 @@ import { Component } from '@angular/core';
 import { PAISES } from '../models/paises.model';
 
 interface ConsultaResponse {
-  result : string;
-  documentation? : string;
-  terms_of_use? : string;
-  time_last_update_unix? : number; 
-  time_last_update_utc? : string; 
-  time_next_update_unix? : number; 
-  time_next_update_utc? : string;
-  error_type? : string;
-  base_code? : string;
-  conversion_rates? : { [key: string]: number };
+  moeda_base : string;
+  taxas : { [key: string]: number };
   data_hora_consulta : string;
 }
 
