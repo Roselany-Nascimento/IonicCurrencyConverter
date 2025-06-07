118cbdfb2c238fd3dff18950e82dd34ca22f6942
JulioCesar-A <145122101+JulioCesar-A@users.noreply.github.com>
2025-06-07 20:05:31 -0300

    Update tab1.page.ts
    Correção na interface ConversaoResponse

--- ioniccurrencyconverterapp/src/app/tab1/tab1.page.ts
+++ ioniccurrencyconverterapp/src/app/tab1/tab1.page.ts
@@ -3,19 +3,11 @@ import { PAISES } from '../models/paises.model';
 
 
 interface ConversaoResponse {
-  result : string;
-  documentation? : string;
-  terms_of_use? : string;
-  time_last_update_unix? : number; 
-  time_last_update_utc? : string; 
-  time_next_update_unix? : number; 
-  time_next_update_utc? : string;
-  base_code? : string; 
-  target_code? : string; 
-  conversion_rate? : number;
-  conversion_result? : number;
+  moeda_base : string; 
+  moeda_destino : string; 
+  taxa : number;
+  valor_convertido : number;
   data_hora_conversao : string;
-  error_type? : string;
 }
 
 @Component({
